e64c9a1ec04b107a9a68d9f6df81191d4d51afae
Thibault Morisse <thibault.morisse@etu.enseeiht.fr>
2012-03-08 12:52:53 +0100

    Ajouts tests. Ne fonctionnent pas

--- s_auth.rb
+++ s_auth.rb
@@ -1,7 +1,7 @@
 # encoding: UTF-8
 
 require 'sinatra'
-$: << File.join(File.dirname(__FILE__),"","middleware")
+$: << File.join(File.dirname(__FILE__))#,"","middleware")
 #require 'my_middleware'
 
 #use RackCookieSession
@@ -22,6 +22,30 @@ get '/s_auth/register' do
 	erb :"s_auth/register", :locals => {:error => error}
 end
 
+post '/s_auth/register' do
+	user=User.new
+	user.login = params[:login]
+  user.password = params[:password]
+  user.save
+	if user.valid?
+		redirect 's_auth/registered'
+	else
+		# A FAIRE
+		errlog = u.errors.messages[:login]
+	  errpass = u.errors.messages[:password]
+		if errlog.include?("has already been taken")
+			redirect "/s_auth/register?error=Login_already_taken. Please entry an another one."
+		elsif errlog.include?("can't be blank")
+			redirect "/s_auth/register?error=The login is empty.Please_entry_a_login."
+		else
+			redirect "/s_auth/register?error=Test dans tous les cas"
+		end
+	end
+end
+
+get '/s_auth/registered' do
+	erb :'s_auth/registered'
+end
 
 
 
--- spec/s_auth_spec.rb
+++ spec/s_auth_spec.rb
@@ -29,10 +29,10 @@ describe "Authentication" do
 			it "should register a user if the login and the password are ok" do
 			  params={'login' => 'tmorisse', 'password' => 'motDePasse'}
 				post '/s_auth/register', params
-				puts "post passe"
 				last_response.should be_redirect
-				
 			end
+
+			User.all.each{|user| user.destroy}
 
 		end
 
@@ -41,30 +41,24 @@ describe "Authentication" do
 	context "The login and/or the password is/are not ok" do
 
 		it "should redirect to /s_auth/register if the password is empty" do
-				params={'login' => 'tmorisse', 'password' => ''}
-				post '/s_auth/register', params
-				last_response.should be_redirect
+			params={"login" => 'tmorisse', "password" => nil}
+			post '/s_auth/register', params
+			#last_response.should be_redirect
+			follow_redirect!
+      last_request.should '/s_auth/register'
+			#last_response.headers["Location"].should == 'http://example.org/s_auth/appli_cliente_1/new?info=Password_Empty'
 		end
 
 		it "should redirect to /s_auth/register if the login is empty" do
-				params={'login' => '', 'password' => 'motDePasse'}
-				post '/s_auth/register', params
-				last_response.should be_redirect
+			params={"login" => nil, "password" => 'motDePasse'}
+			post '/s_auth/register', params
+			#last_response.should be_redirect
+			follow_redirect!
+	     last_request.should '/s_auth/register'
+			#last_response.headers["Location"].should == 'http://example.org/s_auth/appli_cliente_1/new?info=Login_Empty'
 		end
 
-
-
-
 	end
-
-
-
-	context "A user is registered" do
-
-	end
-
-
-
 
 
 end
