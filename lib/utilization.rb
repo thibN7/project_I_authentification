ee630e66f84593c51d44ee373d6d75aea93ef037
Thibault Morisse <thibault.morisse@etu.enseeiht.fr>
2012-03-19 12:59:41 +0100

    Suppression doublon

--- lib/utilization.rb
+++
@@ -1,6 +0,0 @@
-require 'active_record'
-
-class Utilization < ActiveRecord::Base
-  belongs_to :user			# foreign key - user_id
-  belongs_to :application	# foreign key - application_id
-end
--- spec/spec_helper.rb
+++
@@ -1,5 +0,0 @@
-require 'rspec'
-require 'active_record'
-$:.unshift File.join(File.dirname(__FILE__),'..','lib')
-
-require_relative '../database'
